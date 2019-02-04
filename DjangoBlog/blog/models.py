21a3500a59db552916687e1fba6b5b05eea7ce76
satyaSK <satyajit.k@somaiya.edu>
2019-02-05 00:14:49 +0530

    Blog in progress

--- DjangoBlog/blog/models.py
+++ DjangoBlog/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 # Create your models here.
 class Post(models.Model): #To add post into DB via shell ---> post_1 = Post(content = "", title = "", author = ""  ---> post_1.save()
@@ -10,5 +11,11 @@ class Post(models.Model): #To add post into DB via shell ---> post_1 = Post(cont
 	author = models.ForeignKey(User, on_delete = models.CASCADE)
 
 	def __str__(self):
-		return self.title 
+		return self.title
+
+	def get_absolute_url(self):
+		return reverse('post-detail', kwargs={'pk': self.pk})
+		
+
+
 
